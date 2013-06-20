7018f5da84e938b18f4bfdf4b0215d5b74e3cfdd
Jakub Sokołowski <panswiata@gmail.com>
2013-06-20 13:14:03 +0200

    renaimed main netdev module file

--- kernel/netdev_main.c
+++ kernel/netdev_main.c
@@ -9,8 +9,10 @@
 #include <linux/fs.h>
 #include <linux/cdev.h>
 #include <linux/slab.h>     /* kmalloc, kzalloc, kfree and so on */
+#include <linux/netlink.h>  /* for netlink sockets */
 
 #include "fo.h"
+#include "netlink.h"
 
 MODULE_VERSION("0.0.1");
 MODULE_LICENSE("GPL v2");
@@ -309,10 +311,6 @@ static void netdev_cleanup(void) {
     return;
 }
 
-int netlink_connect() {
-    return 0;
-}
-
 static int __init netdev_init(void) /* Constructor */
 {
     int err;
@@ -369,6 +367,10 @@ static int __init netdev_init(void) /* Constructor */
     printk(KERN_DEBUG "netdev: This is my new device: %d, %d\n",
                         MAJOR(devno), 
                         MINOR(devno));
+
+    // TEST NETLINK
+    init_netlink();
+
     return 0; /* success */
     
 fail:
@@ -379,6 +381,7 @@ fail:
 static void __exit netdev_exit(void) /* Destructor */
 {
     netdev_cleanup();
+    exit_netlink();
     return;
 }
 
