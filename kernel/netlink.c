c564bff8224e69d50ce61b82ecba1490ca6e4e45
Jakub Sokołowski <panswiata@gmail.com>
2013-07-15 23:23:02 +0200

    improved formatting, added freeing of sk_buff in send and recv functions

--- kernel/netlink.c
+++ kernel/netlink.c
@@ -17,47 +17,53 @@ int netlink_echo(int pid, char *p_msg) {
     int msg_size = strlen(p_msg);
     int rvalue;
 
+    printk(KERN_INFO "netlink: received msg payload: %s\n", p_msg);
+
     /* allocate memory for sk_buff, no flags */
     p_skb_out = nlmsg_new(msg_size, 0);
 
     if(!p_skb_out) {
-        printk(KERN_ERR "netlink: Failed to allocate new skb\n");
-        return -1;
+        printk(KERN_ERR "netlink: failed to allocate new sk_buff!\n");
+        rvalue = -1;
+        goto skbfree;
     } 
-    p_nlh = nlmsg_put(p_skb_out, 0, 0, NLMSG_DONE, msg_size, 0);  
+    p_nlh = nlmsg_put(p_skb_out, 0, 0, MSGTYPE_CONTROL_ECHO, msg_size, 0);  
 
     NETLINK_CB(p_skb_out).dst_group = 0; /* not in mcast group */
 
     strncpy(nlmsg_data(p_nlh) ,p_msg ,msg_size);
 
+    /* send messsage, nlmsg_unicast will take care of freeing p_skb_out */
     rvalue = nlmsg_unicast(p_nl_sk,p_skb_out,pid);
 
     if(rvalue>0) {
-        printk(KERN_INFO "netlink: Error while sending bak to user\n");
+        printk(KERN_ERR "netlink: error while replying to process\n");
         return -1;
     }
 
-    return 1; /* success */
+    return 0; /* success */
+skbfree:
+    nlmsg_free(p_skb_out);
+    return -1;
 }
 
 /* TODO this will have to be modified to receive responses from server process,
  * find the appropriate wait queue and release it once the reply is assigned
  * to appropriate variables in struct describing one pending file operaion */
-void netlink_recv(struct sk_buff *skb) {
+void netlink_recv(struct sk_buff *p_skb) {
     struct nlmsghdr *p_nlh;
     short msgtype;
 
     printk(KERN_INFO "netlink: Entering: %s\n", __FUNCTION__);
 
-    p_nlh = (struct nlmsghdr*)skb->data;
-    printk(KERN_INFO "netlink: received msg payload: %s\n", (char*)nlmsg_data(p_nlh));
+    p_nlh = (struct nlmsghdr*)p_skb->data;
 
-    pid = p_nlh->nlmsg_pid; /*pid of sending process */
+    pid     = p_nlh->nlmsg_pid;  /* pid of sending process, also, port id */
     msgtype = p_nlh->nlmsg_type;
 
-    printk(KERN_DEBUG "netlink: msg from pid = %d\n", pid);
-    printk(KERN_DEBUG "netlink: msg type: %d\n", msgtype);
+    printk(KERN_DEBUG "netlink: msg type: %d, from pid: %d\n", msgtype, pid);
     if ( pid == 0 ) { /* message from kernel to kernel, disregard */
+        printk(KERN_ERR "netlink: received message from kernel, pid: %d\n", pid);
         return;
     }
 
@@ -93,40 +99,49 @@ int netlink_send(short msgtype) {
     int msg_size;
     int rvalue;
 
-    if ( pid == -1 ) { /* don't send anything if we don't have the pid yet */
+    if ( pid == 0 ) { /* don't send anything if we don't have the pid yet */
+        printk(KERN_ERR "netlink: wrong pid: %d\n", pid);
         return -1;
     }
 
     if (msgtype == MSGTYPE_CONTROL_ECHO) {
         p_msg = "ECHO";
     } else {
-        p_msg = "DEFAULT";
+        p_msg = "Not implemented yet.";
     }
     msg_size = strlen(p_msg);
 
     p_skb_out = nlmsg_new(msg_size, 0);
 
     if(!p_skb_out) {
-        printk(KERN_ERR "Failed to allocate new skb\n");
-        return -1;
+        printk(KERN_ERR "netlink: failed to allocate new sk_buff!\n");
+        goto skbfree;
     } 
-    p_nlh = nlmsg_put(p_skb_out, 0, 0, NLMSG_DONE, msg_size, 0);  
+    p_nlh = nlmsg_put(p_skb_out, 0, 0, msgtype, msg_size, 0);  
 
     NETLINK_CB(p_skb_out).dst_group = 0; /* not in mcast group */
 
     strncpy(nlmsg_data(p_nlh) ,p_msg ,msg_size);
-
+    
+    /* send messsage, nlmsg_unicast will take care of freeing p_skb_out */
     rvalue = nlmsg_unicast(p_nl_sk, p_skb_out, pid);
 
-    if(rvalue>0)
+    if( rvalue > 0 ) {
         printk(KERN_INFO "Error while sending bak to user\n");
+        return -1;
+    }
 
-    return 0;
+    return 0; /* success */
+skbfree:
+    nlmsg_free(p_skb_out);
+    return -1;
 }
 
 int netlink_init(void) {
-    // new structure for configuring netlink socket changed in linux kernel 3.8
-    // http://stackoverflow.com/questions/15937395/netlink-kernel-create-is-not-working-with-latest-linux-kernel
+    /* NOTE:
+     * new structure for configuring netlink socket changed in linux kernel 3.8
+     * http://stackoverflow.com/questions/15937395/netlink-kernel-create-is-not-working-with-latest-linux-kernel
+     */
     struct netlink_kernel_cfg nl_cfg;
     nl_cfg.groups = 0;           // used for multicast
     nl_cfg.flags = 0;            // TODO
@@ -147,7 +162,8 @@ int netlink_init(void) {
 
 void netlink_exit(void) {
     printk(KERN_INFO "netdev: closing Netlink\n");
-    // TODO will have to close connections of all devices with the server process
+    /* TODO will have to close connections of all devices with the server process
+     * or just close the socket if each device will have it's own process/socket */
 
     /* can only be done once the process releases the socket, otherwise
      * kernel will refuse to unload the module */
