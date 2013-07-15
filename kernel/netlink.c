248a591853391fc65bbe022565b497fbf3bc2db4
Jakub Sokołowski <panswiata@gmail.com>
2013-07-15 13:46:42 +0200

    added guards against kernel communicating with itself through netlink, creating a loop

--- kernel/netlink.c
+++ kernel/netlink.c
@@ -1,67 +1,73 @@
 #include <linux/module.h>
 #include <linux/netlink.h>  /* for netlink sockets */
-#include <linux/skbuff.h>
 #include <net/sock.h>
 
 #include "netlink.h"
-#include "msgtype.h"
+#include "protocol.h"
 
-/* defines the protocol used, we want our own protocol */
-#define NETLINK_USER 31
-
-struct sock *nl_sk = NULL;
+struct sock *p_nl_sk = NULL;
 /* TODO this will have to be part of a struct that will contain information
  * about the device this driver will pretend to be received from the server
  * process */
-int pid;
+int pid = -1;
 
 /* TODO this will have to be modified to receive responses from server process,
  * find the appropriate wait queue and release it once the reply is assigned
  * to appropriate variables in struct describing one pending file operaion */
 void netlink_recv(struct sk_buff *skb) {
-    struct nlmsghdr *nlh;
+    struct nlmsghdr *p_nlh;
     struct sk_buff *skb_out;
     short msgtype;
     int msg_size;
     char *msg = "Device registered";
     int res;
 
-    printk(KERN_INFO "Entering: %s\n", __FUNCTION__);
+    printk(KERN_INFO "netlink: Entering: %s\n", __FUNCTION__);
 
     msg_size = strlen(msg);
 
-    nlh = (struct nlmsghdr*)skb->data;
-    printk(KERN_INFO "Netlink received msg payload: %s\n", (char*)nlmsg_data(nlh));
-    pid = nlh->nlmsg_pid; /*pid of sending process */
-    msgtype = nlh->nlmsg_type;
+    p_nlh = (struct nlmsghdr*)skb->data;
+    printk(KERN_INFO "netlink: received msg payload: %s\n", (char*)nlmsg_data(p_nlh));
+
+    pid = p_nlh->nlmsg_pid; /*pid of sending process */
+    msgtype = p_nlh->nlmsg_type;
+
+    printk(KERN_DEBUG "netlink: msg from pid = %d\n", pid);
+    if ( pid == 0 ) { /* message from kernel to kernel, disregard */
+        return;
+    }
 
     skb_out = nlmsg_new(msg_size, 0);
 
     if(!skb_out) {
-        printk(KERN_ERR "Failed to allocate new skb\n");
+        printk(KERN_ERR "netlink: Failed to allocate new skb\n");
         return;
     } 
-    nlh = nlmsg_put(skb_out, 0, 0, NLMSG_DONE, msg_size, 0);  
+    p_nlh = nlmsg_put(skb_out, 0, 0, NLMSG_DONE, msg_size, 0);  
 
     NETLINK_CB(skb_out).dst_group = 0; /* not in mcast group */
 
-    strncpy(nlmsg_data(nlh) ,msg ,msg_size);
+    strncpy(nlmsg_data(p_nlh) ,msg ,msg_size);
 
-    res = nlmsg_unicast(nl_sk,skb_out,pid);
+    res = nlmsg_unicast(p_nl_sk,skb_out,pid);
 
     if(res>0)
-        printk(KERN_INFO "Error while sending bak to user\n");
+        printk(KERN_INFO "netlink: Error while sending bak to user\n");
 
 }
 
 /* TODO should receive not only msgtype but also a buffer with properly 
  * formatted arguments to the file operation from the netdev_fo_OP_send_req */
 int netlink_send(short msgtype) {
-    struct nlmsghdr *nlh;
+    struct nlmsghdr *p_nlh;
     struct sk_buff *skb_out;
     char *msg;
     int msg_size;
     int res;
+
+    if ( pid == -1 ) { /* don't send anything if we don't have the pid yet */
+        return -1;
+    }
 
     if (msgtype == MSGTYPE_NETDEV_ECHO) {
         msg = "ECHO";
@@ -76,13 +82,13 @@ int netlink_send(short msgtype) {
         printk(KERN_ERR "Failed to allocate new skb\n");
         return -1;
     } 
-    nlh = nlmsg_put(skb_out, 0, 0, NLMSG_DONE, msg_size, 0);  
+    p_nlh = nlmsg_put(skb_out, 0, 0, NLMSG_DONE, msg_size, 0);  
 
     NETLINK_CB(skb_out).dst_group = 0; /* not in mcast group */
 
-    strncpy(nlmsg_data(nlh) ,msg ,msg_size);
+    strncpy(nlmsg_data(p_nlh) ,msg ,msg_size);
 
-    res = nlmsg_unicast(nl_sk,skb_out,pid);
+    res = nlmsg_unicast(p_nl_sk, skb_out, pid);
 
     if(res>0)
         printk(KERN_INFO "Error while sending bak to user\n");
@@ -94,29 +100,30 @@ int netlink_init(void) {
     // new structure for configuring netlink socket changed in linux kernel 3.8
     // http://stackoverflow.com/questions/15937395/netlink-kernel-create-is-not-working-with-latest-linux-kernel
     struct netlink_kernel_cfg nl_cfg;
-    nl_cfg.groups = 0,// used for multicast
-    nl_cfg.flags = 0, // TODO
-    nl_cfg.input = netlink_recv, // pointer to function that will receive data
+    nl_cfg.groups = 0;           // used for multicast
+    nl_cfg.flags = 0;            // TODO
+    nl_cfg.input = netlink_recv; // pointer to function that will receive data
 
-    printk("Entering: %s\n", __FUNCTION__);
+    printk("netdev: Netlink protocol: %d\n", NETLINK_PROTOCOL);
 
-    nl_sk = netlink_kernel_create(&init_net, NETLINK_USER, &nl_cfg);
+    p_nl_sk = netlink_kernel_create(&init_net, NETLINK_PROTOCOL, &nl_cfg);
 
-    if(!nl_sk) {
+    if(!p_nl_sk) {
         printk(KERN_ALERT "Error creating socket.\n");
         return -10;
     }
 
+    printk("netdev: Netlink setup complete\n");
     return 0;
 }
 
 void netlink_exit(void) {
-    printk(KERN_INFO "exiting hello module\n");
+    printk(KERN_INFO "netdev: closing Netlink\n");
     // TODO will have to close connections of all devices with the server process
 
     /* can only be done once the process releases the socket, otherwise
      * kernel will refuse to unload the module */
-    netlink_kernel_release(nl_sk);
+    netlink_kernel_release(p_nl_sk);
 }
 
 MODULE_LICENSE("GPL");
