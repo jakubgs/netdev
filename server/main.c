9727891eed724d1d68a23a6a4370f14acf1d61de
Jakub Sokołowski <panswiata@gmail.com>
2013-07-15 13:42:49 +0200

    moved netlink socket setup to a separate function and changed global variables to be local ones

--- server/main.c
+++ server/main.c
@@ -2,6 +2,7 @@
 #include <unistd.h> /* close */
 #include <stdlib.h> /* malloc */
 #include <stdio.h>  /* printf */
+#include <errno.h>
 #include <sys/socket.h>
 #include <linux/netlink.h>
 
@@ -9,77 +10,87 @@
 
 #define MAX_PAYLOAD 1024 /* maximum payload size*/
 
-int netlink_setup(int sock_fd) {
-    struct sockaddr_nl src_addr;
+int netlink_setup() {
+    struct sockaddr_nl *p_src_addr;
+    int sock_fd = -1;
     int rvalue = 0;
 
     /* create socket for netlink
      * int socket(int domain, int type, int protocol); */
-    sock_fd = socket(AF_NETLINK, SOCK_RAW, NETLINK_PROTOCOL);
+    sock_fd = socket(PF_NETLINK, SOCK_RAW, NETLINK_PROTOCOL);
 
     /* check if socket was actually created */
-    if ( sock_fd < 0 )
-        return -1;
-
-    /* zerou out struct before using it */
-    memset(&src_addr, 0, sizeof(src_addr));
-
-    src_addr.nl_family = AF_NETLINK;
-    src_addr.nl_pid = getpid(); /* self pid */
-
-    /* src_addr has to be casted to (struct sockaddr*) for compatibility with
-     * IPv4 and IPv6 structures, sockaddr_in and sockaddr_in6. */
-    rvalue = bind(sock_fd, (struct sockaddr*)&src_addr, sizeof(src_addr));
-
-    if ( rvalue < 0 ) {
+    if ( sock_fd < 0 ) {
+        perror("socket error");
         return -1;
     }
 
-    return 1;
+    p_src_addr = (struct sockaddr_nl *)malloc(sizeof(*p_src_addr));
+    memset(p_src_addr, 0, sizeof(*p_src_addr));
+    p_src_addr->nl_family = AF_NETLINK;
+    p_src_addr->nl_pid = getpid(); /* self pid */
+
+    /* p_src_addr has to be casted to (struct sockaddr*) for compatibility with
+     * IPv4 and IPv6 structures, sockaddr_in and sockaddr_in6. */
+    rvalue = bind(sock_fd, (struct sockaddr*)p_src_addr, sizeof(*p_src_addr));
+
+    if ( rvalue < 0 ) {
+        perror("socket error");
+        return -1;
+    }
+
+    /* connect is not needed since we are not using TCP but a raw socket */
+    return sock_fd;
 }
 
 int main() {
     struct sockaddr_nl dest_addr;
-    struct nlmsghdr *nlh = NULL;
+    struct nlmsghdr *p_nlh = NULL;
     struct iovec iov;
-    struct msghdr msg;
-    int sock_fd;
+    struct msghdr *p_msg; // needs to be global or a pointer
+    int sock_fd = -1;
+    int rvalue = 0;
 
-    /* setup socket */
-    if ( netlink_setup(sock_fd) < 0 )
-        return -1;
+    printf("Netlink protocol: %d\n", NETLINK_PROTOCOL);
+    sock_fd = netlink_setup();
+    printf("sock_fd: %d\n", sock_fd);
 
-    /* zerou out struct before using it */
+    /* zero out struct before using it */
     memset(&dest_addr, 0, sizeof(dest_addr));
 
     dest_addr.nl_family = AF_NETLINK;
     dest_addr.nl_pid = 0; /* For Linux Kernel */
     dest_addr.nl_groups = 0; /* unicast */
 
-    nlh = (struct nlmsghdr *)malloc(NLMSG_SPACE(MAX_PAYLOAD));
-    memset(nlh, 0, NLMSG_SPACE(MAX_PAYLOAD));
-    nlh->nlmsg_len = NLMSG_SPACE(MAX_PAYLOAD);
-    nlh->nlmsg_pid = getpid();
-    nlh->nlmsg_flags = 0;
-    nlh->nlmsg_type = MSGTYPE_NETDEV_REGISTER;
+    p_nlh = (struct nlmsghdr *)malloc(NLMSG_SPACE(MAX_PAYLOAD));
+    memset(p_nlh, 0, NLMSG_SPACE(MAX_PAYLOAD));
+    p_nlh->nlmsg_len = NLMSG_SPACE(MAX_PAYLOAD);
+    p_nlh->nlmsg_pid = getpid();
+    p_nlh->nlmsg_flags = 0;
+    p_nlh->nlmsg_type = MSGTYPE_NETDEV_REGISTER;
 
-    strcpy(NLMSG_DATA(nlh), "Register device for sharing");
+    strcpy(NLMSG_DATA(p_nlh), "Register device for sharing");
 
-    iov.iov_base = (void *)nlh;
-    iov.iov_len = nlh->nlmsg_len;
-    msg.msg_name = (void *)&dest_addr;
-    msg.msg_namelen = sizeof(dest_addr);
-    msg.msg_iov = &iov;
-    msg.msg_iovlen = 1;
+    iov.iov_base = (void *)p_nlh;
+    iov.iov_len = p_nlh->nlmsg_len;
+    p_msg = (struct msghdr *)malloc(sizeof(struct msghdr));
+    p_msg->msg_name = (void *)&dest_addr;
+    p_msg->msg_namelen = sizeof(dest_addr);
+    p_msg->msg_iov = &iov;
+    p_msg->msg_iovlen = 1;
 
     printf("Sending message to kernel\n");
-    sendmsg(sock_fd,&msg,0);
+    sendmsg(sock_fd,p_msg,0);
     printf("Waiting for message from kernel\n");
 
     /* Read message from kernel */
     for ( ; ; ) {
-        recvmsg(sock_fd, &msg, 0);
-        printf("Received message payload: %s\n", (char*)NLMSG_DATA(nlh));
+        rvalue = recvmsg(sock_fd, p_msg, 0);
+        printf("rvalue: %d\n", rvalue);
+        if (rvalue <= 0) {
+            break;
+        }
+        printf("Received message payload: %s\n", (char*)NLMSG_DATA(p_nlh));
     }
 
     close(sock_fd);
