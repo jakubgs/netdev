8a0b035b4250f16d52073396171d7e65fa6f95fd
Jakub Sokołowski <panswiata@gmail.com>
2013-07-23 21:49:07 +0200

    extended arguments of netlink_send

--- kernel/netlink.h
+++ kernel/netlink.h
@@ -3,9 +3,11 @@
 
 #include <linux/skbuff.h>
 
+#include "netdevmgm.h"
+
 int netlink_echo(int pid, char *msg);
 void netlink_recv(struct sk_buff *skb);
-int netlink_send(short msgtype);
+int netlink_send(struct netdev_data *nddata, short msgtype, char *buff, size_t bufflen);
 int netlink_init(void);
 void netlink_exit(void);
 
