02e3ec4fce1f516c330fdd921d79d648b5364998
Jakub Sokołowski <panswiata@gmail.com>
2013-07-23 21:50:28 +0200

    moved netdev_class and netdev_devno here so they are globally accessible, added four new functions of rmanagement of netdev devices from netlink

--- kernel/netdevmgm.h
+++ kernel/netdevmgm.h
@@ -1,17 +1,25 @@
-#ifndef _NETDEV_H
-#define _NETDEV_H
+#ifndef _NETDEVMGM_H
+#define _NETDEVMGM_H
 
 #include <linux/cdev.h>
 #include <linux/module.h>   /* for THIS_MODULE */
+#include <linux/hashtable.h>
+
+#include "protocol.h"
+
+extern dev_t netdev_devno; /* __u32, unsigned 32bit type, 12 bit for majro, 20 minor */
+extern struct class *netdev_class;
 
 /* for now we use only one, but in future we will use one for every device */
 /* TODO this structure will need a semafor/spinlock */
 struct netdev_data {
     /* TODO might need to use uint32_t for the sake of network communication */
     unsigned int nlpid; /* pid of the process that created this device */
-    char *name; /* name of the device provided by the process */
-    struct cdev cdev;
+    char *devname; /* name of the device provided by the process */
+    struct cdev *cdev; /* internat struct representing char devices */
     struct device *device;
+    bool ready; /* set to false if netlink connection is lost */
+    struct hlist_node hnode; /* to use with hastable */
     /* TODO
      * this struct will containt more data especially about host of the device
      * and device name, major, minor, and process pid of the server that will
@@ -20,5 +28,11 @@ struct netdev_data {
 };
 
 int netdev_create(int nlpid, char *name);
+int netdev_destroy(int nlpid, struct netdev_data *nddata);
+void netdev_htable_init(void);
+struct netdev_data* netdev_find(int nlpid);
+void netdev_end(void);
 
 #endif
+
+
