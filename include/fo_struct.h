017ba466ed27cf06f2bee8511b0212bb818a0ab2
Jakub Sokołowski <panswiata@gmail.com>
2013-07-29 00:31:04 +0200

    added rvalue since we need to somehow get back the return value of file operation

--- include/fo_struct.h
+++ include/fo_struct.h
@@ -8,109 +8,134 @@
 struct s_fo_llseek {
     loff_t offset;
     int whence;
+    loff_t rvalue;
 };
 struct s_fo_read {
     char __user *data;
-    size_t c;
+    size_t size;
     loff_t *offset;
+    ssize_t rvalue;
 };
 struct s_fo_write {
     const char __user *data;
-    size_t c;
+    size_t size;
     loff_t *offset;
+    ssize_t rvalue;
 };
 struct s_fo_aio_read {
     struct kiocb *a;
     const struct iovec *b;
     unsigned long c;
     loff_t offset;
+    ssize_t rvalue;
 };
 struct s_fo_aio_write {
     struct kiocb *a;
     const struct iovec *b;
     unsigned long c;
     loff_t offset;
+    ssize_t rvalue;
 };
 struct s_dev_fo_poll {
     struct poll_table_struct *wait;
+    unsigned long rvalue;
 };
 struct s_fo_unlocked_ioctl {
     unsigned int b;
     unsigned long c;
+    long rvalue;
 };
 struct s_fo_compat_ioctl {
     unsigned int b;
     unsigned long c;
+    long rvalue;
 };
 struct s_fo_mmap {
     struct vm_area_struct *b;
+    int rvalue;
 };
 struct s_fo_open {
     struct inode *inode;
+    int rvalue;
 };
 struct s_fo_flush {
     fl_owner_t id;
+    int rvalue;
 };
 struct s_fo_release {
     struct inode *inode;
+    int rvalue;
 };
 struct s_fo_fsync {
     loff_t b;
     loff_t c;
     int d;
+    int rvalue;
 };
 struct s_fo_aio_fsync {
     struct kiocb *a;
     int b;
+    int rvalue;
 };
 struct s_fo_fasync {
     int a;
     int c;
+    int rvalue;
 };
 struct s_fo_lock {
     int b;
+    int rvalue;
 };
 struct s_fo_sendpage {
     struct page *b;
     int c;
-    size_t d;
+    size_t size;
     loff_t *offset;
     int f;
+    ssize_t rvalue;
 };
 struct s_fo_get_unmapped_area{
     unsigned long b;
     unsigned long c;
     unsigned long d;
     unsigned long e;
+    unsigned long rvalue;
 };
 struct s_fo_check_flags {
     int a;
+    int rvalue;
 };
 struct s_fo_flock {
     int b;
+    int rvalue;
 };
 struct s_fo_splice_write {
     struct pipe_inode_info *a;
     loff_t *offset;
-    size_t d;
+    size_t size;
     unsigned int e;
+    ssize_t rvalue;
 };
 struct s_fo_splice_read {
     loff_t *b;
     struct pipe_inode_info *c;
-    size_t d;
+    size_t size;
     unsigned int e;
+    ssize_t rvalue;
 };
 struct s_fo_setlease {
     long b;
+    int rvalue;
 };
 struct s_fo_fallocate {
     int b;
     loff_t offset;
     loff_t len;
+    long rvalue;
 };
 struct s_fo_show_fdinfo {
     struct seq_file *a;
+    int rvalue;
 };
 
 #endif /* _FO_STRUCT_H */
