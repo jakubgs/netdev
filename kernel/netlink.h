a10573794a6c543d981afa9407b579f5becdc93f
Jakub Sokołowski <panswiata@gmail.com>
2013-07-31 01:17:46 +0200

    added netlink_send_fo since it's a specific type of message

--- kernel/netlink.h
+++ kernel/netlink.h
@@ -4,10 +4,12 @@
 #include <linux/skbuff.h>
 
 #include "netdevmgm.h"
+#include "fo.h"
 
 int netlink_echo(int pid, char *msg);
 void netlink_recv(struct sk_buff *skb);
 int netlink_send(struct netdev_data *nddata, short msgtype, char *buff, size_t bufflen);
+int netlink_send_fo(struct netdev_data *nddata, struct fo_req *req);
 int netlink_init(void);
 void netlink_exit(void);
 
