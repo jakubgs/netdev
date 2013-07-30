d6c9f9e7e590acae07eae594675deccc2e3624bf
Jakub Sokołowski <panswiata@gmail.com>
2013-07-31 01:19:12 +0200

    sequence number is nedded for echo to use the same oen for reply

--- kernel/netlink.h
+++ kernel/netlink.h
@@ -6,7 +6,7 @@
 #include "netdevmgm.h"
 #include "fo.h"
 
-int netlink_echo(int pid, char *msg);
+int netlink_echo(int pid, int seq, char *msg);
 void netlink_recv(struct sk_buff *skb);
 int netlink_send(struct netdev_data *nddata, short msgtype, char *buff, size_t bufflen);
 int netlink_send_fo(struct netdev_data *nddata, struct fo_req *req);
