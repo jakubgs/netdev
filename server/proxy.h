3e71a44d8f82499e1fb04e7fc6a31a3029147973
Jakub Sokołowski <panswiata@gmail.com>
2013-08-01 17:26:00 +0200

    proxy_dev structure contains all the information necessary for the process that will pass messages frome one kernel to another, all fiel descriptors and addresses as well as minor/major numbers and device names

---
+++ server/proxy.h
@@ -0,0 +1,47 @@
+#ifndef _PROXY_H
+#define _PROXY_H
+
+#include <linux/netlink.h>  /* sockaddr_nl */
+#include <netinet/in.h>     /* sockaddr_in */
+#include <sys/un.h>         /* sockaddr_un */
+
+#define MAX_PAYLOAD 1024 /* maximum payload size*/
+
+struct proxy_dev {
+    int pid; /* pid od the proxy process */
+    int nl_fd; /* netlink file descryptor */
+    int rm_fd; /* remote file descryptor */
+    int us_fd[2]; /* unix socket pair, first one is for reading */
+    struct sockaddr_nl *nl_src_addr;
+    struct sockaddr_nl *nl_dst_addr;
+    struct sockaddr_in *rm_addr;
+    struct sockaddr_un *us_addr;
+    int rm_portaddr;
+    char *rm_ipaddr;
+    char *remote_dev_name;
+    char *dummy_dev_name;
+    int remote_major;
+    int remote_minor;
+    int dummy_major;
+    int dummy_minor;
+    struct nlmsghdr *nlh; /* for reusing */
+    struct msghdr *msgh; /* for reusing */
+    struct iovec *iov; /* for reusing */
+};
+
+int proxy_netlink_setup(struct proxy_dev *pdev);
+int proxy_register_device(struct proxy_dev *pdev, struct msghdr *p_msg, struct iovec *p_iov);
+int proxy_unregister_device(void);
+void proxy_setup_signals();
+int proxy_setup_unixsoc(struct proxy_dev *pdev);
+struct proxy_dev * proxy_alloc_dev(int connfd);
+void proxy_client(struct proxy_dev *pdev);
+void proxy_server(int connfd);
+void proxy_sig_hup(int signo);
+int proxy_loop();
+int proxy_control(struct proxy_dev *pdev);
+int proxy_handle_remote(struct proxy_dev *pdev);
+int proxy_handle_netlink(struct proxy_dev *pdev);
+int netlink_unregister_dev(struct proxy_dev *pdev);
+
+#endif
