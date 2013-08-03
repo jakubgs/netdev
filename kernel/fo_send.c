1c9f5ec0f7c9a2617dd446619fa59ae907b5ac88
Jakub Sokołowski <panswiata@gmail.com>
2013-08-03 17:17:37 +0200

    changed send_fo to fo_send an added fo_recv for generic file operation handling and two other functions for specific handling depending on device type, server or dummy

--- kernel/fo_send.c
+++ kernel/fo_send.c
@@ -1,4 +1,5 @@
 #include <linux/slab.h>
+#include <linux/kthread.h>
 
 #include "fo.h"
 #include "protocol.h"
@@ -10,10 +11,14 @@
  * to the server process */
 /* TODO might require a struct that will also hold the wait queue to unblock waiting
  * send_req function once receiving loop gets the response */
-int send_fo (short msgtype, struct netdev_data *nddata, void *args, size_t size) {
+int fo_send(short msgtype, struct netdev_data *nddata, void *args, size_t size)
+{
     int rvalue = 0;
     void *buffer = NULL;
     struct fo_req *req = NULL;
+
+    /* increment reference counter of netdev_data */
+    ndmgm_get(nddata);
 
     req = kmem_cache_alloc(nddata->queue_pool, GFP_KERNEL);
 
@@ -21,17 +26,17 @@ int send_fo (short msgtype, struct netdev_data *nddata, void *args, size_t size)
         printk(KERN_ERR "senf_fo: failed to allocate queue_pool\n");
         return -ENOMEM;
     }
-
+    
     req->msgtype = msgtype;
     req->args = args;
     req->size = size;
     init_completion(&req->comp);
-    printk(KERN_DEBUG "send_fo: size = %zu\n", req->size);
+    printk(KERN_DEBUG "fo_send: size = %zu\n", req->size);
 
     buffer = kzalloc(req->size, GFP_KERNEL);
 
     if (!buffer) {
-        printk(KERN_ERR "send_fo: failed to allocate buffer\n");
+        printk(KERN_ERR "fo_send: failed to allocate buffer\n");
         rvalue = -ENOMEM;
         goto out;
     }
@@ -46,112 +51,202 @@ int send_fo (short msgtype, struct netdev_data *nddata, void *args, size_t size)
     /* wait for completion, it will be signaled once a reply is received */
     wait_for_completion(&req->comp);
     rvalue = req->rvalue;
-
+    
 out:
     kmem_cache_free(nddata->queue_pool, req);
+    ndmgm_put(nddata); /* decrease references to netdev_data */
     return rvalue;
 }
 
+int fo_recv(struct sk_buff *skb)
+{
+    struct netdev_data *nddata = NULL;
+    struct task_struct *task = NULL;
+    struct nlmsghdr *nlh = NULL;
+
+    nlh = nlmsg_hdr(skb);
+
+    nddata = ndmgm_find(nlh->nlmsg_pid);
+    if (IS_ERR(nddata)) {
+        printk(KERN_ERR "fo_execute: failed to find device for pid = %d\n",
+                nlh->nlmsg_pid);
+        return 1; /* failute */
+    }
+
+    if (nddata->dummy) {
+        return fo_complete(nddata, nlh, skb); /* find fo in queue and complete it */
+    } else {
+        /* crate a new thread since we want to return control to the
+         * server process so it doesn't wait needlesly */
+        task = kthread_run(&fo_execute, (void*)skb, "fo_execute");
+        if (IS_ERR(task)) {
+            printk("fo_recv: failed to create thread for file operation, errpr = %ld\n", PTR_ERR(task));
+            return 1; /* failure */
+        }
+    }
+
+    return 0; /* success */
+}
+
+int fo_complete(
+    struct netdev_data *nddata,
+    struct nlmsghdr *nlh,
+    struct sk_buff *skb)
+{
+    /* TODO find file operation in the queue, pass result and complete */
+    return 1; /* success */
+}
+
+/* this function will be executed as a new thread with kthread_run */
+int fo_execute(void *data)
+{
+    struct netdev_data *nddata = NULL;
+    struct nlmsghdr *nlh = NULL;
+    struct sk_buff *skb = NULL;
+
+    skb = data; /* cast to sk_buff */
+    nlh = nlmsg_hdr(skb);
+
+    nddata = ndmgm_find(nlh->nlmsg_pid);
+    if (IS_ERR(nddata)) {
+        printk(KERN_ERR "fo_execute: failed to find device for pid = %d\n",
+                nlh->nlmsg_pid);
+        do_exit(-1); /* this is a thread */
+    }
+
+    /* TODO execute appropriate file operation and then send back
+     * the result to the server in userspace */
+
+    do_exit(0); /* success */
+}
+
 /* functions for sending and receiving file operations */
-loff_t netdev_fo_llseek_send_req (struct file *filp, loff_t offset, int whence) {
+loff_t netdev_fo_llseek_send_req (struct file *filp, loff_t offset, int whence)
+{
     return -EIO;
 }
-ssize_t netdev_fo_read_send_req (struct file *filp, char __user *data, size_t size, loff_t *offset) {
+ssize_t netdev_fo_read_send_req (struct file *filp, char __user *data, size_t size, loff_t *offset)
+{
     struct s_fo_read args = {
         .data = NULL,
         .size = size,
         .offset = offset
     };
 
-    if ( send_fo(MSGT_FO_READ, filp->private_data, &args, sizeof(args)) ) {
+    if ( fo_send(MSGT_FO_READ, filp->private_data, &args, sizeof(args)) ) {
         return args.rvalue;
     }
     return -EIO;
 }
-ssize_t netdev_fo_write_send_req (struct file *filp, const char __user *data, size_t c, loff_t *offset) {
+ssize_t netdev_fo_write_send_req (struct file *filp, const char __user *data, size_t size, loff_t *offset)
+{
     struct s_fo_write args = {
         .data = NULL,
         .size = size,
         .offset = offset
     };
 
-    if ( send_fo(MSGT_FO_WRITE, filp->private_data, &args, sizeof(args))) {
+    if ( fo_send(MSGT_FO_WRITE, filp->private_data, &args, sizeof(args))) {
         return args.rvalue;
     }
     return -EIO;
 }
-size_t netdev_fo_aio_read_send_req (struct kiocb *a, const struct iovec *b, unsigned long c, loff_t offset) {
+size_t netdev_fo_aio_read_send_req (struct kiocb *a, const struct iovec *b, unsigned long c, loff_t offset)
+{
     return -EIO;
 }
-ssize_t netdev_fo_aio_write_send_req (struct kiocb *a, const struct iovec *b, unsigned long c, loff_t d) {
+ssize_t netdev_fo_aio_write_send_req (struct kiocb *a, const struct iovec *b, unsigned long c, loff_t d)
+{
     return -EIO;
 }
-unsigned int netdev_fo_poll_send_req (struct file *filp, struct poll_table_struct *wait) {
+unsigned int netdev_fo_poll_send_req (struct file *filp, struct poll_table_struct *wait)
+{
     return -EIO;
 }
-long netdev_fo_unlocked_ioctl_send_req (struct file *filp, unsigned int b, unsigned long c) {
+long netdev_fo_unlocked_ioctl_send_req (struct file *filp, unsigned int b, unsigned long c)
+{
     return -EIO;
 }
-long netdev_fo_compat_ioctl_send_req (struct file *filp, unsigned int b, unsigned long c) {
+long netdev_fo_compat_ioctl_send_req (struct file *filp, unsigned int b, unsigned long c)
+{
     return -EIO;
 }
-int netdev_fo_mmap_send_req (struct file *filp, struct vm_area_struct *b) {
+int netdev_fo_mmap_send_req (struct file *filp, struct vm_area_struct *b)
+{
     return -EIO;
 }
-int netdev_fo_open_send_req (struct inode *inode, struct file *filp) {
-    struct s_fo_open args = {
+int netdev_fo_open_send_req (struct inode *inode, struct file *filp)
+{
+    struct s_fo_open args =
+    {
         .inode = inode
     };
 
-    if ( send_fo(MSGT_FO_OPEN, filp->private_data, &args, sizeof(args)) ) {
+    if ( fo_send(MSGT_FO_OPEN, filp->private_data, &args, sizeof(args)) )
+    {
         return args.rvalue;
     }
     return 0;
 }
-int netdev_fo_flush_send_req (struct file *filp, fl_owner_t id) {
-    send_fo(MSGT_FO_FLUSH, filp->private_data, NULL, 0);
+int netdev_fo_flush_send_req (struct file *filp, fl_owner_t id)
+{
+    fo_send(MSGT_FO_FLUSH, filp->private_data, NULL, 0);
     return 0;
 }
-int netdev_fo_release_send_req (struct inode *a, struct file *filp) {
-    send_fo(MSGT_FO_RELEASE, filp->private_data, NULL, 0);
+int netdev_fo_release_send_req (struct inode *a, struct file *filp)
+{
+    fo_send(MSGT_FO_RELEASE, filp->private_data, NULL, 0);
     return 0;
 }
-int netdev_fo_fsync_send_req (struct file *filp, loff_t b, loff_t offset, int d) {
+int netdev_fo_fsync_send_req (struct file *filp, loff_t b, loff_t offset, int d)
+{
     return -EIO;
 }
-int netdev_fo_aio_fsync_send_req (struct kiocb *a, int b) {
+int netdev_fo_aio_fsync_send_req (struct kiocb *a, int b)
+{
     return -EIO;
 }
-int netdev_fo_fasync_send_req (int a, struct file *filp, int c) {
+int netdev_fo_fasync_send_req (int a, struct file *filp, int c)
+{
     return -EIO;
 }
-int netdev_fo_lock_send_req (struct file *filp, int b, struct file_lock *c) {
+int netdev_fo_lock_send_req (struct file *filp, int b, struct file_lock *c)
+{
     return -EIO;
 }
-ssize_t netdev_fo_sendpage_send_req (struct file *filp, struct page *b, int c, size_t d, loff_t *offset, int f) {
+ssize_t netdev_fo_sendpage_send_req (struct file *filp, struct page *b, int c, size_t d, loff_t *offset, int f)
+{
     return -EIO;
 }
-unsigned long netdev_fo_get_unmapped_area_send_req (struct file *filp, unsigned long b, unsigned long c,unsigned long d, unsigned long e) {
+unsigned long netdev_fo_get_unmapped_area_send_req (struct file *filp, unsigned long b, unsigned long c,unsigned long d, unsigned long e)
+{
     return -EIO;
 }
-int netdev_fo_check_flags_send_req (int a) {
+int netdev_fo_check_flags_send_req (int a)
+{
     return -EIO;
 }
-int netdev_fo_flock_send_req (struct file *filp, int b, struct file_lock *c) {
+int netdev_fo_flock_send_req (struct file *filp, int b, struct file_lock *c)
+{
     return -EIO;
 }
-ssize_t netdev_fo_splice_write_send_req (struct pipe_inode_info *a, struct file *filp, loff_t *offset, size_t d, unsigned int e) {
+ssize_t netdev_fo_splice_write_send_req (struct pipe_inode_info *a, struct file *filp, loff_t *offset, size_t d, unsigned int e)
+{
     return -EIO;
 }
-ssize_t netdev_fo_splice_read_send_req (struct file *filp, loff_t *offset, struct pipe_inode_info *c, size_t d, unsigned int e) {
+ssize_t netdev_fo_splice_read_send_req (struct file *filp, loff_t *offset, struct pipe_inode_info *c, size_t d, unsigned int e)
+{
     return -EIO;
 }
-int netdev_fo_setlease_send_req (struct file *filp, long b, struct file_lock **c) {
+int netdev_fo_setlease_send_req (struct file *filp, long b, struct file_lock **c)
+{
     return -EIO;
 }
-long netdev_fo_fallocate_send_req (struct file *filp, int b, loff_t offset, loff_t len) {
+long netdev_fo_fallocate_send_req (struct file *filp, int b, loff_t offset, loff_t len)
+{
     return -EIO;
 }
-int netdev_fo_show_fdinfo_send_req (struct seq_file *a, struct file *filp) {
+int netdev_fo_show_fdinfo_send_req (struct seq_file *a, struct file *filp)
+{
     return -EIO;
 }
