c70d2a20ab554794830e5effcc7efd6328d6a2db
Jakub Sokołowski <panswiata@gmail.com>
2013-08-04 15:47:37 +0200

    added a structure for received netlink messages and generic send/recv functions

--- server/netlink.h
+++ server/netlink.h
@@ -3,8 +3,16 @@
 
 #include "proxy.h"
 
+struct netlink_message {
+    int msgtype;
+    size_t size;
+    void * payload;
+};
+
 int netlink_setup(struct proxy_dev *pdev);
 int netlink_reg_dummy_dev(struct proxy_dev *pdev);
 int netlink_reg_remote_dev(struct proxy_dev *pdev);
+int netlink_send( struct proxy_dev *pdev, void *buff, size_t bsize, int msgtype, int flags);
+struct netlink_message * netlink_recv(struct proxy_dev *pdev);
 
 #endif
