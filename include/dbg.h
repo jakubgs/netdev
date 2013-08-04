be076e81eb8c29cf719b2362285dda188873e57a
Jakub Sokołowski <panswiata@gmail.com>
2013-08-04 15:20:18 +0200

    changed dbg.h to include a debug functions for kernel

--- include/dbg.h
+++ include/dbg.h
@@ -1,30 +1,11 @@
-#ifndef __dbg_h__
-#define __dbg_h__
+#ifndef _DBG_H
+#define _DBG_H
 
-#include <stdio.h>
-#include <errno.h>
-#include <string.h>
 
 #ifdef NDEBUG
 #define debug(M, ...)
 #else
-#define debug(M, ...) fprintf(stderr, "DEBUG %s:%d: " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#define debug(M, ...) printk(KERN_DEBUG "NETDEV_DEBUG: %s:%d:\t\t" M "\n", __FUNCTION__, __LINE__, ##__VA_ARGS__)
 #endif
 
-#define clean_errno() (errno == 0 ? "None" : strerror(errno))
-
-#define log_err(M, ...) fprintf(stderr, "[ERROR] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
-
-#define log_warn(M, ...) fprintf(stderr, "[WARN] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
-
-#define log_info(M, ...) fprintf(stderr, "[INFO] (%s:%d) " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
-
-#define check(A, M, ...) if(!(A)) { log_err(M, ##__VA_ARGS__); errno=0; goto error; }
-
-#define sentinel(M, ...)  { log_err(M, ##__VA_ARGS__); errno=0; goto error; }
-
-#define check_mem(A) check((A), "Out of memory.")
-
-#define check_debug(A, M, ...) if(!(A)) { debug(M, ##__VA_ARGS__); errno=0; goto error; }
-
-#endif
+#endif /* _DBG_H */
