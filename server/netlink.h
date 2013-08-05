6262716a46c352927b3c92a1a6acfa526ddd9622
Jakub Sokołowski <panswiata@gmail.com>
2013-08-05 22:05:20 +0200

    it's pointless to use my own structure for netlink messages when I can use nlmsghdr, also split _send into _send that takes nlmsghdr and _send_msg that takes a buffer to be payload and confirms the delivery

--- server/netlink.h
+++ server/netlink.h
@@ -3,16 +3,11 @@
 
 #include "proxy.h"
 
-struct netlink_message {
-    int msgtype;
-    size_t size;
-    void * payload;
-};
-
 int netlink_setup(struct proxy_dev *pdev);
 int netlink_reg_dummy_dev(struct proxy_dev *pdev);
 int netlink_reg_remote_dev(struct proxy_dev *pdev);
-int netlink_send( struct proxy_dev *pdev, void *buff, size_t bsize, int msgtype, int flags);
-struct netlink_message * netlink_recv(struct proxy_dev *pdev);
+int netlink_send(struct proxy_dev *pdev, struct nlmsghdr *nlh);
+int netlink_send_msg(struct proxy_dev *pdev, void *buff, size_t bsize, int msgtype, int flags);
+struct nlmsghdr * netlink_recv(struct proxy_dev *pdev);
 
 #endif
