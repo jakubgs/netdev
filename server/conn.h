38a60bd37d9bef394348bc2937b6df31240015eb
Jakub Sokołowski <panswiata@gmail.com>
2013-08-05 22:04:13 +0200

    removed version checking since it will be done when establishing connection witht he server, added genric functions for sending and receiving that use loops

--- server/conn.h
+++ server/conn.h
@@ -2,12 +2,15 @@
 #define _CONN_H
 
 #include "proxy.h"
+#include "netprotocol.h"
 
+int conn_send(struct proxy_dev *pdev, struct netdev_header *ndhead);
+struct netdev_header * conn_recv(struct proxy_dev *pdev);
 int conn_server(struct proxy_dev *pdev);
 int conn_client(struct proxy_dev *pdev);
-int conn_send_version(struct proxy_dev *pdev);
 int conn_send_dev_reg(struct proxy_dev *pdev);
-int conn_recv_version(struct proxy_dev *pdev);
 int conn_recv_dev_reg(struct proxy_dev *pdev);
+int recvall(int conn_fd, struct msghdr *msgh, int size);
+int sendall(int conn_fd, struct msghdr *msgh, int size);
 
 #endif
