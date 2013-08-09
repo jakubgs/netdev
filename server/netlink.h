630f0ae649daf73204ee8f795d7139d6c03ad033
Jakub Sokołowski <panswiata@gmail.com>
2013-08-09 15:55:17 +0200

    added send_nlh for easier netlink header sending, send_msg now just creates a netlink header and uses send_nlh

--- server/netlink.h
+++ server/netlink.h
@@ -8,6 +8,7 @@ int netlink_reg_dummy_dev(struct proxy_dev *pdev);
 int netlink_reg_remote_dev(struct proxy_dev *pdev);
 int netlink_send(struct proxy_dev *pdev, struct nlmsghdr *nlh);
 int netlink_send_msg(struct proxy_dev *pdev, void *buff, size_t bsize, int msgtype, int flags);
+int netlink_send_nlh(struct proxy_dev *pdev, struct nlmsghdr *nlh);
 struct nlmsghdr * netlink_recv(struct proxy_dev *pdev);
 
 #endif
