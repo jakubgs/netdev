d7c0cf6b39db45eadf842e0dca64431e24b23b8b
Jakub Sokołowski <panswiata@gmail.com>
2013-08-09 15:47:03 +0200

    better formatting that aligns vertically messages

--- include/dbg.h
+++ include/dbg.h
@@ -5,7 +5,7 @@
 #ifdef NDEBUG
 #define debug(M, ...)
 #else
-#define debug(M, ...) printk(KERN_DEBUG "NETDEV_DEBUG: %s:%d:\t\t" M "\n", __FUNCTION__, __LINE__, ##__VA_ARGS__)
+#define debug(M, ...) printk(KERN_DEBUG "NETDEV_DEBUG: %20s:%4d:\t\t" M "\n", __FUNCTION__, __LINE__, ##__VA_ARGS__)
 #endif
 
 #endif /* _DBG_H */
