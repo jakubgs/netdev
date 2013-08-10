7843b3621f834f7a05b1f81fbd6e0a8a8dd37424
Jakub Sokołowski <panswiata@gmail.com>
2013-08-10 19:54:38 +0200

    added send_skb fo easy sending from fo_execute

--- kernel/netlink.h
+++ kernel/netlink.h
@@ -11,6 +11,7 @@ void netlink_recv(struct sk_buff *skb);
 int netlink_create_dummy(int pid, int seq, char *buff, size_t bsize);
 int netlink_ackmsg(struct netdev_data *nddata, int seq, short msgtype);
 int netlink_send(struct netdev_data *nddata, int seq, short msgtype, int flags, void *buff, size_t bufflen);
+int netlink_send_skb(struct netdev_data *nddata, struct sk_buff *skb);
 int netlink_send_fo(struct netdev_data *nddata, struct fo_req *req);
 int netlink_init(void);
 void netlink_exit(void);
