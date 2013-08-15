1f44bbc4b1661e31d14dbe9242af76a9f590623b
Jakub Sokołowski <panswiata@gmail.com>
2013-08-15 21:44:51 +0200

    added netlink_pack_skb for adding big payloads to skb like file reads and writes

--- kernel/netlink.h
+++ kernel/netlink.h
@@ -12,6 +12,7 @@ int netlink_create_dummy(int pid, int seq, char *buff, size_t bsize);
 int netlink_ackmsg(struct netdev_data *nddata, int seq, short msgtype);
 int netlink_send(struct netdev_data *nddata, int seq, short msgtype, int flags, void *buff, size_t bufflen);
 int netlink_send_skb(struct netdev_data *nddata, struct sk_buff *skb);
+struct sk_buff * netlink_pack_skb( struct nlmsghdr *nlh, void *buff, size_t bufflen);
 int netlink_send_fo(struct netdev_data *nddata, struct fo_req *req);
 int netlink_init(void);
 void netlink_exit(void);
