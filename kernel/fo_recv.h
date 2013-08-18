fd126208a02e707a0ae06e4621a93d88a1ca8c26
Jakub Sokołowski <panswiata@gmail.com>
2013-08-18 21:45:37 +0200

    all recv functions now take struct fo_access as first argument
    this is necessary for support of multiple processes accessing the same
    device file

--- kernel/fo_recv.h
+++ kernel/fo_recv.h
@@ -2,34 +2,35 @@
 #define _FO_RECV_H
 
 #include "fo_comm.h"
+#include "fo_access.h"
 
 /* functions for executing file operations sent from client */
-int ndfo_recv_llseek(struct netdev_data *nddata, struct fo_req *req);
-int ndfo_recv_read(struct netdev_data *nddata, struct fo_req *req);
-int ndfo_recv_write(struct netdev_data *nddata, struct fo_req *req);
-int ndfo_recv_aio_read(struct netdev_data *nddata, struct fo_req *req);
-int ndfo_recv_aio_write(struct netdev_data *nddata, struct fo_req *req);
-int ndfo_recv_poll(struct netdev_data *nddata, struct fo_req *req);
-int ndfo_recv_unlocked_ioctl(struct netdev_data *nddata, struct fo_req *req);
-int ndfo_recv_compat_ioctl(struct netdev_data *nddata, struct fo_req *req);
-int ndfo_recv_mmap(struct netdev_data *nddata, struct fo_req *req);
-int ndfo_recv_open(struct netdev_data *nddata, struct fo_req *req);
-int ndfo_recv_flush(struct netdev_data *nddata, struct fo_req *req);
-int ndfo_recv_release(struct netdev_data *nddata, struct fo_req *req);
-int ndfo_recv_fsync(struct netdev_data *nddata, struct fo_req *req);
-int ndfo_recv_aio_fsync(struct netdev_data *nddata, struct fo_req *req);
-int ndfo_recv_fasync(struct netdev_data *nddata, struct fo_req *req);
-int ndfo_recv_lock(struct netdev_data *nddata, struct fo_req *req);
-int ndfo_recv_sendpage(struct netdev_data *nddata, struct fo_req *req);
-int ndfo_recv_get_unmapped_area(struct netdev_data *nddata, struct fo_req *req);
-int ndfo_recv_check_flags(struct netdev_data *nddata, struct fo_req *req);
-int ndfo_recv_flock(struct netdev_data *nddata, struct fo_req *req);
-int ndfo_recv_splice_write(struct netdev_data *nddata, struct fo_req *req);
-int ndfo_recv_splice_read(struct netdev_data *nddata, struct fo_req *req);
-int ndfo_recv_setlease(struct netdev_data *nddata, struct fo_req *req);
-int ndfo_recv_fallocate(struct netdev_data *nddata, struct fo_req *req);
-int ndfo_recv_show_fdinfo(struct netdev_data *nddata, struct fo_req *req);
+int ndfo_recv_llseek(struct fo_access *acc, struct fo_req *req);
+int ndfo_recv_read(struct fo_access *acc, struct fo_req *req);
+int ndfo_recv_write(struct fo_access *acc, struct fo_req *req);
+int ndfo_recv_aio_read(struct fo_access *acc, struct fo_req *req);
+int ndfo_recv_aio_write(struct fo_access *acc, struct fo_req *req);
+int ndfo_recv_poll(struct fo_access *acc, struct fo_req *req);
+int ndfo_recv_unlocked_ioctl(struct fo_access *acc, struct fo_req *req);
+int ndfo_recv_compat_ioctl(struct fo_access *acc, struct fo_req *req);
+int ndfo_recv_mmap(struct fo_access *acc, struct fo_req *req);
+int ndfo_recv_open(struct fo_access *acc, struct fo_req *req);
+int ndfo_recv_flush(struct fo_access *acc, struct fo_req *req);
+int ndfo_recv_release(struct fo_access *acc, struct fo_req *req);
+int ndfo_recv_fsync(struct fo_access *acc, struct fo_req *req);
+int ndfo_recv_aio_fsync(struct fo_access *acc, struct fo_req *req);
+int ndfo_recv_fasync(struct fo_access *acc, struct fo_req *req);
+int ndfo_recv_lock(struct fo_access *acc, struct fo_req *req);
+int ndfo_recv_sendpage(struct fo_access *acc, struct fo_req *req);
+int ndfo_recv_get_unmapped_area(struct fo_access *acc, struct fo_req *req);
+int ndfo_recv_check_flags(struct fo_access *acc, struct fo_req *req);
+int ndfo_recv_flock(struct fo_access *acc, struct fo_req *req);
+int ndfo_recv_splice_write(struct fo_access *acc, struct fo_req *req);
+int ndfo_recv_splice_read(struct fo_access *acc, struct fo_req *req);
+int ndfo_recv_setlease(struct fo_access *acc, struct fo_req *req);
+int ndfo_recv_fallocate(struct fo_access *acc, struct fo_req *req);
+int ndfo_recv_show_fdinfo(struct fo_access *acc, struct fo_req *req);
 
-extern int (*netdev_recv_fops[])(struct netdev_data*, struct fo_req*);
+extern int (*netdev_recv_fops[])(struct fo_access*, struct fo_req*);
 
 #endif /* _FO_RECV_H */
