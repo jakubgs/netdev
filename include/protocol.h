835f8461464249353a704e130584e887bc9657b7
Jakub Sokołowski <panswiata@gmail.com>
2013-08-19 20:10:12 +0200

    moved all defines delated only to the kernel to netdev.h

--- include/protocol.h
+++ include/protocol.h
@@ -1,35 +1,17 @@
 #ifndef _PROTOCOL_H
 #define _PROTOCOL_H
 
-/* this will be provided by the name variable in netdev_data struct */
-#define NETDEV_NAME             "netdev"
-
-/* this number will be increased every time the core protocol changed */
-#define NETDEV_PROTOCOL_VERSION 1
+/* this number HAS TO BE incremented every time anything in the 
+ * the core protocol is changed */
+#define NETDEV_PROTOCOL_VERSION 2
 #define NETDEV_PROTOCOL_SUCCESS 0 /* 0 will always mean correct */
 #define NETDEV_PROTOCOL_FAILURE -1 /* 0 will always mean failure */
 
 /* size limit of messages sent through sendmsg */
 #define NETDEV_MESSAGE_LIMIT    213000
 
-/* let the kernel decide the major number */
-#define NETDEV_MAJOR            0
-
-/* defines the number which is the power of two which gives the max number
- * of devices this driver will manage and store in the hashtable */
-#define NETDEV_HTABLE_DEV_SIZE  5
-#define NETDEV_HTABLE_ACC_SIZE  5
-#define NETDEV_MAX_DEVICES      32 /* 2 to the power of 5(HTABLE_DEV_SIZE)*/
-#define NETDEV_MAX_OPEN_FILES   32 /* 2 to the power of 5(HTABLE_ACC_SIZE) */
-#define NETDEV_FO_QUEUE_SIZE    64 /* has to be a power of 2 */
-#define NETDEV_REQ_POOL_NAME    "netdev_req_pool" /* for debugging */
 /* defines the default netlink server port */
 #define NETDEV_SERVER_PORT      9999
-
-#define NETDEV_MAXLINE          1024
-
-/* 2nd argument to listen(), max size of listen queue */
-#define NETDEV_LISTENQ          1024
 
 /* defines the netlink bus used, we want our own protocol so we use the 
  * last one of the 32 available buses */
---
+++ kernel/netdev.h
@@ -0,0 +1,22 @@
+#ifndef _NETDEV_H
+#define _NETDEV_H
+
+/* this will be provided by the name variable in netdev_data struct */
+#define NETDEV_NAME             "netdev"
+
+/* let the kernel decide the major number */
+#define NETDEV_MAJOR            0
+
+/* defines the number which is the power of two which gives the max number
+ * of devices this driver will manage and store in the hashtable */
+#define NETDEV_HTABLE_DEV_SIZE  5
+#define NETDEV_HTABLE_ACC_SIZE  5
+#define NETDEV_MAX_DEVICES      32 /* 2 to the power of 5(HTABLE_DEV_SIZE)*/
+#define NETDEV_MAX_OPEN_FILES   32 /* 2 to the power of 5(HTABLE_ACC_SIZE) */
+#define NETDEV_FO_QUEUE_SIZE    64 /* has to be a power of 2 */
+#define NETDEV_REQ_POOL_NAME    "netdev_req_pool" /* for debugging */
+
+/* 2nd argument to listen(), max size of listen queue */
+#define NETDEV_LISTENQ          1024
+
+#endif /* _NETDEV_H */
